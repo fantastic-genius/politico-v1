d87139ae2e303b079eb79a748085b70124f8e782
fantastic-genius <fantastigenius@yahoo.com>
2019-02-01 15:49:13 +0100

    Fix made changes based on review - created edit party and edit office page - improved on my validation in my middleware

--- server/middleware/officeMiddleware.js
+++ server/middleware/officeMiddleware.js
@@ -1,16 +1,36 @@
+import {offices} from "../db/db"
 class OfficeMiddleware{
     createOfficeMiddleware(req, res, next){
-        if(!req.body.type){
+        if(!req.body.type || !(req.body.type).trim()){
             return res.status(400).send({
                 status: 400,
                 error: "Office type not included in the data posted" 
             })
-        }else if(!req.body.name){
+        }else if(!req.body.name || !(req.body.name).trim()){
             return res.status(400).send({
                 status: 400,
                 error: "Office name not included in the data posted" 
             })
+        }else if(!isNaN(parseInt(req.body.type))){
+            return res.status(400).send({
+                status: 400,
+                error: "Office type contains an integer instead of a string" 
+            })
+        }else if(!isNaN(parseInt(req.body.name))){
+            return res.status(400).send({
+                status: 400,
+                error: "Office name contains an integer instead of a string" 
+            })
         }
+
+        offices.map(office => {
+            if(office.name === req.body.name){
+                return res.status(412).send({
+                    status: 412,
+                    error: `${req.body.name} already existed` 
+                })
+            }
+        })
 
         next();
     }
