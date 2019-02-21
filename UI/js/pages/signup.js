cf501ec54562deca60e65c5d5ae68a0b068980a9
fantastic-genius <fantastigenius@yahoo.com>
2019-02-21 10:11:31 +0100

    feat(signup) Consumed signup api - Implemented signup functionality to consume signup api - Implemented cors in apis [Delivers #164103447]

---
+++ UI/js/pages/signup.js
@@ -0,0 +1,75 @@
+//Method to display alert message
+const displayMessage = ((type, msg) => {
+    const alertDiv = document.querySelector('#alert')
+    let msgSpan = ''
+    if(type == 'success'){
+        msgSpan += `<div class='alert-msg alert-success'><span class='close-btn'>&times;</span><strong>Success!</strong> ${msg}</div>`
+    }else if(type == 'warning'){
+        msgSpan += `<span class='alert-msg alert-warning'><span class='close-btn'>&times;</span><strong>Warning!</strong> ${msg}</div>`
+    }else if(type == 'info'){
+        msgSpan += `<div class='alert-msg alert-info'><span class='close-btn'>&times;</span><strong>Info!</strong> ${msg}</div>`
+    }else if(type == 'danger'){
+        msgSpan += `<div class='alert-msg alert-danger'><span class='close-btn'>&times;</span><strong>Error!</strong> ${msg}</div>`
+    }
+ 
+    alertDiv.innerHTML = msgSpan
+
+    const closebtn = document.querySelector('.close-btn') 
+    closebtn.addEventListener('click', closeAlert)
+})
+
+
+///----####---SIGNUP START----####----///
+const signUp = (e) => {
+    e.preventDefault();
+    const firstname = document.querySelector('#first-name').value
+    const othername = document.querySelector('#other-name').value
+    const lastname = document.querySelector('#last-name').value
+    const email = document.querySelector('#email').value
+    const phoneNumber = document.querySelector('#phone').value
+    const password = document.querySelector('#password').value
+    const passwordconf = document.querySelector('#password-conf').value
+
+    if(password.length < 8){
+        displayMessage('danger', 'Invalid Password Provided. Password must be not be less than 8 characters')
+    }else if(password !== passwordconf){
+        displayMessage('danger', 'Passwords provided doesn\'t not match')
+    }else{
+        fetch('http://localhost:5000/api/v1/auth/signup', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                firstname,
+                othername,
+                lastname,
+                email,
+                password,
+                phoneNumber
+            })
+        }).then(res => {
+            return res.json()
+        }).then(data => {
+            if(data.status == 201){
+                const msg = `You have successfuly Registered. 
+                            You can log into you acount here  <a href='login.html'>Login</a>`
+                displayMessage('success', msg)
+            }else{
+                displayMessage('success', data.error)
+            }
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
+}
+
+const signupBtn = document.querySelector('#signup')
+
+if(signupBtn){
+    signupBtn.addEventListener('click', signUp)
+}
+
+///----####---SIGNUP END----####----///
--- index.js
+++ index.js
@@ -8,12 +8,15 @@ import voteRouter from "./server/routes/voteRoutes"
 import petitionRouter from "./server/routes/petitionRoutes"
 import swaggerUi from  "swagger-ui-express"
 import YAML from "yamljs"
+import path from 'path'
+import cors from 'cors'
  
 const PORT = 5000
 const app = express()
+const router = express.Router()
 const debugg = debug('app:')
 const swaggerDocument = YAML.load("./politico-api.yaml")
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(partyRouter)
@@ -22,6 +25,13 @@ app.use(authRouter)
 app.use(voteRouter)
 app.use(petitionRouter)
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.use('/', router)
+app.use(express.static(__dirname + '/UI'))
+app.use(express.static(__dirname + '/images'))
 
 if(!module.parent){
     app.listen(process.env.PORT || PORT, () => {
